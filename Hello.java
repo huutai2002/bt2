b37a27c09c845334e97260624c27c9d044c3674a
huutai2002 <nhtai0560@gmail.com>
2023-09-28 16:05:46 +0700

    edit file Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
-	public static void main(String []args) {
-		System.out.println("Hello Java");
-	}
+	public static void main(String []args){
+        for(int i=0; i<10; i++){
+            System.out.println(i+1 );
+        }
+     }
 }
